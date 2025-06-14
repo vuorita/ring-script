1c96b3516140ca6ec5e9e9aca1ecb0600b85db10
vuorita <thevuorinen@gmail.com>
2025-06-14 03:22:22 +0300

    Update ring.js
    Changing local address to internet address..

--- ring.js
+++ ring.js
@@ -1,6 +1,8 @@
 async function fetchMembers() {
   try {
-    const response = await fetch('members.json');
+    //const response = await fetch('members.json');
+    const response = await fetch('https://vuorita.github.io/ring-script/members.json');
+
     const members = await response.json();
 
     const container = document.getElementById('members-list');
